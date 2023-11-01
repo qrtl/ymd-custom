13aa1eefffb98c6450b7f9ba241966d04a5dd5c9
Yoshi Tashiro <tashiro@quartile.co>
2023-11-01 12:59:39 +0000

    [3888][ADD] mrp_production_procurement_group_cascade

---
+++ mrp_production_procurement_group_cascade/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Production Procurement Group Cascade",
+    "category": "Manufacturing",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["mrp"],
+    "installable": True,
+}
---
+++ mrp_production_procurement_group_cascade/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+With this module, when a manufacturing order is created as a child of another
+manufacturing order, the child order will inherit the parent order's procurement group.
